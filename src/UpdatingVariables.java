18ca9bf0467477bab9b038f365d2d71b68fdd4cf
Jose Luis Olivares Camacho <joselukeolivares@gmail.com>
2023-09-22 17:33:43 -0700

    basics

---
+++ src/UpdatingVariables.java
@@ -0,0 +1,5 @@
+public class UpdatingVariables {
+    public static void main(String[]args){
+        System.out.println("in progress...");
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[]args){
+        int speed = 10;
+        int salary=100;
+        String name="Jose Luis";
+        System.out.println(speed);
+        System.out.println(salary);
+        System.out.println(name);
+    }
+}
