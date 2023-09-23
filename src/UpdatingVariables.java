080f8a6dc0d517c817cfafa287e61ce07dd332da
Jose Luis Olivares Camacho <joselukeolivares@gmail.com>
2023-09-22 17:57:46 -0700

    Variables updated

--- src/UpdatingVariables.java
+++ src/UpdatingVariables.java
@@ -1,5 +1,12 @@
 public class UpdatingVariables {
     public static void main(String[]args){
         System.out.println("in progress...");
+        double salary=1000;
+        salary=salary*1.20;
+        System.out.println(salary);
+        salary=salary-50;
+        System.out.println(salary);
+        salary=salary+(30*2)-45;
+        System.out.println(salary);
     }
 }
